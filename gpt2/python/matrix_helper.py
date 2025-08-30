219553006bc39c6967f1c89e49dfe82f57ba2f60
Michael Williams <michaelwilliams@Michaels-MacBook-Pro.local>
2025-08-29 22:31:58 -0700

    Added mat mul to python matrix helper

--- gpt2/python/matrix_helper.py
+++ gpt2/python/matrix_helper.py
@@ -3,7 +3,7 @@ import numpy as np
 # takes 2d np array and flattens to 1d
 def flatten(m):
     d_in, d_out = m.shape
-    arr = [0 for i in range(d_in * d_out)]
+    arr = np.zeros((d_in * d_out))
     count = 0
 
     for row in range(d_in):
@@ -26,7 +26,7 @@ def transpose(m):
 
 # transpose matrix as 1d flattened array
 def transpose_1d(arr, d_in, d_out):
-    out = [0 for i in range(d_in * d_out)]
+    out = np.zeros((d_in * d_out))
     
     for row in range(d_in):
         for col in range(d_out):
@@ -34,8 +34,21 @@ def transpose_1d(arr, d_in, d_out):
 
     return out
 
-def mat_mul(d_in, d_out):
-    pass
+def mat_mul(m1, m2):
+    m_d_in, m_d_out = m1.shape
+    m2_d_in, m2_d_out = m2.shape
+    # out will be square the smallest dimension of m2
+    out = np.zeros((m_d_in, m2_d_out))
+
+    for row in range(m_d_in):
+        for col2 in range(m2_d_out):
+            s = 0.0
+            for col in range(m_d_out):
+                s += float(m1[row][col] * m2[col][col2])
+            out[row][col2] = s
+
+    return out
+
 
 # create a 2d np array
 m = np.random.randn(2, 3)
@@ -50,3 +63,10 @@ m_flattened_1d = flatten(m)
 print (m_flattened_1d)
 m_tranposed_1d = transpose_1d(m_flattened_1d, 2, 3)
 print(m_tranposed_1d)
+
+m2 = np.random.randn(2, 3)
+m2_t = transpose(m2)
+res = mat_mul(m, m2_t)
+print(res.shape)
+print(res)
+print(m @ m2_t)
